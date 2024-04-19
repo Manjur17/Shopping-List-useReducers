08dc4fbcdaf8c7b1c4bee4e2d6d09d232fb4f885
SK Manjur Ali <skmanjuralli9@gmail.com>
2024-04-19 21:15:15 +0530

    final push movie-search-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react';
+
+// CSS import
 import './App.css'
-import TodoList from "./components/TodoList/TodoList"
+
+// Component imports
+import Navbar from './components/Navbar/Navbar';
+import MainRoutes from './routes/Mainroutes';
+// const MainRoutes = React.lazy(() => import('./routes/Mainroutes'));
+
+// Context import
+import ThemeContext from './context/ThemeContext';
 
 function App() {
 
+  const [theme, setTheme] = useState('dark');
+  
+  useEffect(() => {
+    const userTheme = localStorage.getItem('app-theme');
+    if(userTheme != null) {
+      setTheme(userTheme);
+    }
+  }, []);
+
   return (
     <>
-      <TodoList />
+      <ThemeContext.Provider value={{theme, setTheme}}>
+        <div id='app-wrapper' data-theme={theme}>
+          <Navbar />
+          <MainRoutes />
+        </div> 
+      </ThemeContext.Provider>
     </>
   )
 }
